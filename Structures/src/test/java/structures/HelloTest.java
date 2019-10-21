cfde8f88040e2eafb768fd0242868bab18b81e73
tom <tom.martzolf@etu.unilim.fr>
2019-10-21 16:13:48 +0200

    init

--- Structures/src/test/java/structures/HelloTest.java
+++ Structures/src/test/java/structures/HelloTest.java
@@ -1,12 +1,14 @@
 package structures;
 
 public class HelloTest {
+    private static final String BONJOURE = "Bonjoure";
+
     private static void echo(String str) {
         System.out.println(str);
     }
 
     public static void main(String[] args) {
-        String[] array = { "Bonjoure", "Limoges", "!" };
+        String[] array = { BONJOURE, "Limoges", "!" };
         for (String word : array) {
             HelloTest.echo(word);
         }
