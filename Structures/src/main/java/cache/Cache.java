1e0739164befb27ac92953f28bc8e1bb3ca1d2ed
tom <tom.martzolf@etu.unilim.fr>
2019-10-25 11:50:13 +0200

    Ancienne France

---
+++ Structures/src/main/java/cache/Cache.java
@@ -0,0 +1,30 @@
+package cache;
+
+public abstract class Cache<K,V> {
+	
+	protected Cache(int capacity) {
+		
+	}
+	
+	public V get(K key ) {
+		return null;
+	}
+	
+	public void put(K key, V value) {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return null;
+		
+	}
+	
+	protected abstract int size();
+	protected abstract V getValue(K key);
+	protected abstract void removeEntry();
+	protected abstract void insertEntry( K key, V value);
+	protected abstract void reorder ( K key);
+	protected abstract K[] toArray();
+	protected abstract void clear(); 
+}
