e58a9aa45258b5ab2ec085d17b9bfb8ba15b409e
tom <tom.martzolf@etu.unilim.fr>
2020-01-29 19:02:08 +0100

    test frere

--- Structures/src/main/java/cache/Cache.java
+++ Structures/src/main/java/cache/Cache.java
@@ -3,13 +3,11 @@ package cache;
 import java.util.Arrays;
 
 public abstract class Cache<K,V> {
-	
 	protected int capacity;
 	
 	protected Cache(int capacity) {
 		this.capacity = capacity;
 	}
-	
 	public V get(K key ) {
 		if(getValue(key) != null) {
 			reorder(key);
@@ -17,14 +15,12 @@ public abstract class Cache<K,V> {
 		}
 		return null;
 	}
-	
 	public void put(K key, V value) {
 		if(this.size() == this.capacity) {
 			removeEntry();
 		}
 		insertEntry(key, value);
 	}
-	
 	@Override
 	public String toString() {
 		return Arrays.toString(toArray());
